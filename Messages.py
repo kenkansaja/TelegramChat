7e08fa7b04470b68c9fd9c441765cfe6a045f176
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-08 06:24:04 +0700

    Update Messages.py

--- Messages.py
+++ Messages.py
@@ -1,11 +1,11 @@
-m_start = 'Halo!\nAnda berada dalam obrolan kencan anonim!\n'
-               'Tekan tombol di bawah untuk mulai mengobrol dengan lawan bicara.'
-               'Jika Anda ingin mengenal seseorang lebih baik tekan tombol suka dan dengan simpati timbal balik '
-               'Anda akan dapat mengetahui nama panggilan mitra. Jika lawan bicara tidak sesuai dengan keinginan Anda,'
-               'lalu tekan tombol tidak suka dan mulai obrolan baru.\n'
-               'PERHATIAN!\nAgar bot berhasil bekerja, Anda harus memiliki nama panggilan,'
-               'periksa pengaturan Anda dan pastikan Anda telah mencantumkannya!\n'
-               'Untuk menghentikan bot, panggil perintah /stop.'
+m_start = 'Halo!\nAnda berada dalam obrolan kencan anonim!\n'\
+               'Tekan tombol di bawah untuk mulai mengobrol dengan lawan bicara.'\
+               'Jika Anda ingin mengenal seseorang lebih baik tekan tombol suka dan dengan simpati timbal balik '\
+               'Anda akan dapat mengetahui nama panggilan mitra. Jika lawan bicara tidak sesuai dengan keinginan Anda,'\
+               'lalu tekan tombol tidak suka dan mulai obrolan baru.\n'\
+               '\nPERHATIAN! \nAgar bot berhasil bekerja, Anda harus memiliki nama panggilan,'\
+               'periksa pengaturan Anda dan pastikan Anda telah mencantumkannya!\n'\
+               'Untuk menghentikan bot, panggil perintah /stop.'\
                'Harap dicatat bahwa semua data Anda akan dihapus, jika tidak, Anda akan tetap berada di database.'
 
 m_is_not_free_users = 'Maaf, tetapi saat ini tidak ada pengguna lain.'\
@@ -38,5 +38,5 @@ like_str = '\U0001F44D Suka'
 
 
 def m_all_like (x):
-    return 'Teman bicara menyukai Anda\n' + 'Nama pengguna:' + str (x) + \
+    return 'Teman bicara menyukai Anda\n' + 'Nama pengguna:' + str (x) +\
            '\nSemoga berhasil dengan komunikasi Anda!\nTerima kasih telah bersama kami!'

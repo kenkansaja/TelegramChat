cd4d590d0826236cd584b530a15c5a62e01e6533
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-07 23:11:24 +0700

    Create Messages.py

---
+++ Messages.py
@@ -0,0 +1,42 @@
+m_start = 'Halo! \ nAnda berada dalam obrolan kencan anonim! \ n' \
+               'Tekan tombol di bawah untuk mulai mengobrol dengan lawan bicara. '\
+               'Jika Anda ingin mengenal seseorang lebih baik - tekan tombol suka dan dengan simpati timbal balik' \
+               'Anda akan dapat mengetahui nama panggilan mitra. Jika lawan bicara tidak sesuai dengan keinginan Anda, '\
+               'lalu tekan tombol tidak suka dan mulai obrolan baru. \ n' \
+               'PERHATIAN! Agar bot berhasil bekerja, Anda harus memiliki nama panggilan, '\
+               'periksa pengaturan Anda dan pastikan Anda telah mencantumkannya! \ n' \
+               'Untuk menghentikan bot, panggil perintah / stop. '\
+               'Harap dicatat bahwa semua data Anda akan dihapus, jika tidak, Anda akan tetap berada di database.'
+
+m_is_not_free_users = 'Maaf, tetapi saat ini tidak ada pengguna gratis. '\
+                   'Segera setelah pengguna lain masuk, kami akan menghubungkan Anda!'
+m_is_connect = 'Koneksi dibuat. Sampaikan salam kepada lawan bicara Anda!'
+
+m_play_again = 'Ingin mengobrol dengan orang lain?'
+
+m_is_not_user_name = 'Maaf, bot kami hanya dapat berkomunikasi jika Anda memiliki nama pengguna'
+
+m_good_bye = 'Bot: Selamat tinggal, kami akan senang melihat Anda lagi!'
+
+m_disconnect_user = 'Bot: Penelepon Anda telah terputus'
+
+m_failed = 'Bot: Terjadi kesalahan!'
+
+m_like = 'Bot: Pilihan bagus!'
+
+m_dislike_user = 'Bot: Dialog Berakhir'
+
+m_dislike_user_to = 'Bot: Orang lain tidak menyukai Anda, maaf'
+
+m_send_some_messages = 'Bot: Tidak dapat meneruskan pesan sendiri'
+
+m_has_not_dialog = 'Anda tidak sedang berdialog'
+
+dislike_str = '\ U0001F44E Tidak Suka'
+
+like_str = '\ U0001F44D Suka'
+
+
+def m_all_like (x):
+    return 'Teman bicara menyukai Anda \ n' + 'Nama pengguna:' + str (x) + \
+           '\ nSemoga berhasil dengan komunikasi Anda! \ nTerima kasih telah bersama kami!'

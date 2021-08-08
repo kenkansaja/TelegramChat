08c227b89354c213d50cc1c7d4b3721a31434ec2
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-08 09:59:01 +0700

    Update Messages.py

--- Messages.py
+++ Messages.py
@@ -1,28 +1,28 @@
 m_start = '⚡️ SELAMAT DATANG DI {PROJECT_NAME} ⚡️\n\n_🇮🇩 Semoga Dapat teman atau jodoh\n🇳🇿 I hope you can make a friend or a partner\n\n💬 untuk mencari teman obrolan gunakan button New Chat'
 
-m_is_not_free_users = 'Maaf, tetapi saat ini tidak ada pengguna lain.'\
-                   'Segera setelah pengguna lain masuk, kami akan menghubungkan Anda!'
-m_is_connect = 'Koneksi dibuat. Sampaikan salam kepada lawan bicara Anda!'
+m_is_not_free_users = '❗️ Ma'af Anda tidak berada dalam obrolan\n❗️ Sorry you are not in chat'
+         
+m_is_connect = '✅ Anda sudah berada di dalam obrolan, silahkan kirim chat anda\n✅ You are already in the chat, please send your chat'
 
-m_play_again = 'Ingin mengobrol dengan orang lain?'
+m_play_again = 'Apakah Anda Ingin mengobrol dengan orang lain?\nDo you want to chat with other people?'
 
-m_is_not_user_name = 'Maaf, bot kami hanya dapat berkomunikasi jika Anda memiliki nama pengguna'
+m_is_not_user_name = '❌ Maaf, bot kami hanya dapat berkomunikasi jika Anda memiliki nama pengguna\n❌ Sorry, our bot can only communicate if you have a username'
 
-m_good_bye = 'Bot: Selamat tinggal, kami akan senang melihat Anda lagi!'
+m_good_bye = '❌ Ma'af lawan bicara anda meninggalkan obrolan\n❌ Sorry your interlocutor left the chat'
 
-m_disconnect_user = 'Bot: Penelepon Anda telah terputus'
+m_disconnect_user = 'Ma'af sambungan telah terputus!\nSorry, the connection was lost!'
 
-m_failed = 'Bot: Terjadi kesalahan!'
+m_failed = '❌ Terjadi kesalahan!\n❌ Loss happened'
 
-m_like = 'Bot: Pilihan bagus!'
+m_like = '✅ Pilihan yang bagus!\n✅ Great choice'
 
-m_dislike_user = 'Bot: Dialog Berakhir'
+m_dislike_user = 'Obrolan Telah Berakhir\nChat Has Ended'
 
-m_dislike_user_to = 'Bot: Orang lain tidak menyukai Anda, maaf'
+m_dislike_user_to = 'Lawan tidak menyukai Anda, ma'af\nThe opponent doesn't like you sorry'
 
-m_send_some_messages = 'Bot: Tidak dapat meneruskan pesan sendiri'
+m_send_some_messages = 'Bot tidak bisa meneruskan pesan dari bot\nThe bot could not continue the pan from the bot'
 
-m_has_not_dialog = 'Anda tidak sedang berdialog'
+m_has_not_dialog = 'Anda tidak sedang dalam obrolan\nYou are not in chat'
 
 dislike_str = '\U0001F44E Tidak Suka'
 
@@ -30,5 +30,4 @@ like_str = '\U0001F44D Suka'
 
 
 def m_all_like (x):
-    return 'Teman bicara menyukai Anda\n' + 'Nama pengguna:' + str (x) +\
-           '\nSemoga berhasil dengan komunikasi Anda!\nTerima kasih telah bersama kami!'
+    return 'Teman bicara menyukai Anda\n + Nama pengguna: + str (x) \nSemoga berhasil dengan komunikasi Anda!\nTerima kasih telah bersama kami!\nThe interlocutor likes you\n + Username: + str (x)\nGood luck with your communication!\nThanks for being with us!'

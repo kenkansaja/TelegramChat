17a0d8e8165145877424575bd193d33b124d1bbc
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2022-09-22 06:50:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "musikkugroup") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+GROUP = os.environ.get("GROUP", "aruna_mutual") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "gabutan_escape") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+DB_URI = os.environ.get("DATABASE_URL", "")
